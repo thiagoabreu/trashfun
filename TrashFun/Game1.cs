c1d6e3c9cc9262788401a044e1df6efbad1c1226
Thiago Jedi <thiagoa7@gmail.com>
2014-05-30 17:29:12 -0300

    Retirando GamepadState do Game1

--- TrashFun/Game1.cs
+++ TrashFun/Game1.cs
@@ -67,7 +67,7 @@ namespace TrashFun
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
@@ -84,7 +84,7 @@ namespace TrashFun
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            
             base.Draw(gameTime);
         }
     }
