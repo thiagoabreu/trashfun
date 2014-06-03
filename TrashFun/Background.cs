c9989f4df6a7c43a05751e365fc29d7f95e41072
Thiago Jedi <thiagoa7@gmail.com>
2014-06-03 01:26:20 -0300

    Adicionado documentação

--- TrashFun/Background.cs
+++ TrashFun/Background.cs
@@ -8,24 +8,47 @@ namespace TrashFun
     public class Background
     {
 
-
-        // Textura para o Céu (animado)
+        /// <summary>
+        /// Textura para o céu
+        /// </summary>
         Texture2D animado;
-        // Posições para o Céu (será atualizado)
+        /// <summary>
+        /// Posições para o céu (será atualizado)
+        /// </summary>
         Vector2[] posicoesAnimado;
-        // Largura da Textura do Céu
+        /// <summary>
+        /// Largura da textura do Céu
+        /// </summary>
         int larguraAnimado;
-        // Altura da Textura do Céu
+        /// <summary>
+        /// Altura da Textura do Céu
+        /// </summary>
         int alturaAnimado;
-        // Velocidade da animação do Céu
+        /// <summary>
+        /// Velocidade da animação do Céu
+        /// </summary>
         int velocidade;
 
-        // Textura do Muro ou Outro Bg fixo
+        /// <summary>
+        /// Textura do Muro ou Outro Bg fixo
+        /// </summary>
         Texture2D fixo;
+        /// <summary>
+        /// Posição do bg fixo.
+        /// Deve deixar espaço para o céu.
+        /// </summary>
         Vector2 posicaoFixo;
 
+        /// <summary>
+        /// Guarda o clock da última atualização.
+        /// </summary>
         TimeSpan tempoAnterior;
 
+        /// <summary>
+        /// Construtor. Deve ser chamado para carregar o conteúdo.
+        /// </summary>
+        /// <param name="content">Gerenciador de Conteúdo</param>
+        /// <param name="larguraTela">Utilizado para repetir o bg por toda tela</param>
         public Background(ContentManager content, int larguraTela)
         {
             // TODO: Achar uma velocidade legal pro Céu
@@ -53,6 +76,10 @@ namespace TrashFun
 
         }
 
+        /// <summary>
+        /// Atualiza posição da animação
+        /// </summary>
+        /// <param name="gameTime">Determina o tempo do jogo</param>
         public void Update(GameTime gameTime)
         {
             TimeSpan tempoDecorrido = gameTime.ElapsedGameTime;
@@ -71,6 +98,11 @@ namespace TrashFun
             }
         }
 
+        /// <summary>
+        /// Desenha os backgrounds.
+        /// Primeiro o céu (animado) e depois a paisagem (fixa)
+        /// </summary>
+        /// <param name="spriteBatch">Buffer de vídeo</param>
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
